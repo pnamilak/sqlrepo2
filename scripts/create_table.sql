87c9f3c36bc057ceb9e17f942fda81aa2263cb3f
pnamilak <pranay.sqldba@gmail.com>
2025-06-18 17:36:29 +0530

    updated

--- scripts/create_table.sql
+++ scripts/create_table.sql
@@ -1,9 +1,11 @@
-USE [TennisApp]
+USE [TennisScoringDB]
 GO
 
-/****** Object:  Table [dbo].[Players]    Script Date: 18-06-2025 17:18:45 ******/
+/****** Object:  Table [dbo].[Players]    Script Date: 18-06-2025 17:35:12 ******/
 IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Players]') AND type in (N'U'))
 DROP TABLE [dbo].[Players]
 GO
+
+
 
 
